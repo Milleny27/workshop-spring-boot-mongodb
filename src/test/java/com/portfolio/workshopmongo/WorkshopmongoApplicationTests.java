d7fe1f2e19970d0292c646b227d8082332c1f4d1
Milleny <ellenmilleny@gmail.com>
2022-07-11 20:49:05 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/portfolio/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

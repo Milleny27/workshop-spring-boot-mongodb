96201a8cb3fcf6e30cced26d6e86919c0901490e
Milleny <ellenmilleny@gmail.com>
2022-07-12 20:30:11 -0300

    Atualização de usuário com PUT

--- src/main/java/com/portfolio/workshopmongo/domain/User.java
+++ src/main/java/com/portfolio/workshopmongo/domain/User.java
@@ -1,14 +1,12 @@
 package com.portfolio.workshopmongo.domain;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
 public class User implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -17,7 +15,6 @@ public class User implements Serializable {
 	private String email;
 
 	public User() {
-
 	}
 
 	public User(String id, String name, String email) {
@@ -53,7 +50,10 @@ public class User implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -65,7 +65,11 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
 }
